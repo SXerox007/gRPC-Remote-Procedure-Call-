cc2ab95749dd3c1f077535e3a2a6db43abc90b73
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2018-12-16 00:13:40 +0530

    update

--- slack-bot/table/slackdump/slackdump.go
+++ slack-bot/table/slackdump/slackdump.go
@@ -3,9 +3,9 @@ package slackdump
 import "time"
 
 type SlackDumpData struct {
-	TableName     struct{}  `sql:"current_balances" json:"-"`
-	Id            string    `param:"id" json:"id"`
-	SenderMessage string    `param:"sender_message" json:"sender_message"`
-	ChatBotReply  string    `param:"chatbot_reply" json:"chatbot_reply"`
-	CaptureTime   time.Time `param:"capture_time" json:"capture_time"`
+	TableName     struct{}  `sql:"slack_dump" json:"-"`
+	Id            string    `param:"id"`
+	SenderMessage string    `param:"sender_message"`
+	ChatBotReply  string    `param:"chatbot_reply"`
+	CaptureTime   time.Time `param:"capture_time"`
 }
