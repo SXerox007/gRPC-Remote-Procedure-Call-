74ef4affc9701769a9552cc4144676120ffb7eb0
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2018-12-17 11:38:47 +0530

    update generate.sh

--- blockchain/generate.sh
+++ blockchain/generate.sh
@@ -3,21 +3,12 @@
 protoc blockchain/protos/blockchain.proto --go_out=plugins=grpc:.
 
  #server-run
-go run slack-bot/server/server-app.go
+go run blockchain/server/server-app.go
 # or 
 make server
 
 #client-run
-go run slack-bot/client/client-app.go
+go run blockchain/client/client-app.go
 //# or 
 make client
 
-
-#For get slack package 
-go get github.com/nlopes/slack
-//#source the keys when run hit command
-source slack-bot/secure-keys/keys
-
-
-#We use wit.ai for the msgs
-#create account and just use
